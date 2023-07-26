ed5196487a1798beb9bbaa17dc1ed280c79288d0
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-07-26 21:49:10 +0300

    completed compulsory lab Tuesday

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import React from "react";
-import blogData from "../data/blog";
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
+import data from '../data/blog';
 
-console.log(blogData);
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Your Blog Name" />
+      <About />
+      <ArticleList posts={data.posts} />
     </div>
   );
-}
+};
 
 export default App;
